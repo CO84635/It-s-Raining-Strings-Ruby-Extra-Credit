370c25c655ecd4364786d24911052b0fdc5f38d6
Owen Ceresa <co84635@mail.harpercollege.edu>
2024-12-06 16:22:28 -0600

    Extra credit

---
+++ main.rb
@@ -0,0 +1,25 @@
+business_list = []
+
+puts "Welcome to the Business Sorting Program!"
+
+loop do
+    print "Please enter the name of a business: "
+    business_input = gets.chomp
+
+    if business_input.empty?
+        puts "Input cannot be empty. Please try again."
+        next
+    end
+
+    business_list.push(business_input)
+    business_list.sort!
+
+    puts "\nYour businesses are:"
+    business_list.each { |business| puts business }
+
+    print "\bAnother business? (Yes/No): "
+    choice = gets.chomp.downcase
+    break unless choice == "yes" || choice == "y"
+end
+
+puts "\nThank you for running the Business Sorting Program!"
